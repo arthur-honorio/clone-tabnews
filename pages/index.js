ef5579ed6e7189fcfcbc01eed0401e2bbe89bcb9
Arthur Honorio <hno.arthur@gmail.com>
2024-12-09 01:55:21 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 function Home() {
-    return <p>
-        <h1>Minha moninhaaaa</h1>
-        <h2>Te amooooooo</h2>
+  return (
+    <p>
+      <h1>Minha moninhaaaa</h1>
+      <h2>Te amooooooo</h2>
     </p>
+  );
 }
 
 export default Home;
