e38013b337550261c3f9abd350e189833713e6e9
Arthur Honorio <hno.arthur@gmail.com>
2025-04-23 02:59:50 +0000

    fix `/migrations` endpoint connection bug

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -3,32 +3,41 @@ import { join } from "node:path";
 import database from "infra/database";
 
 export default async function migrations(req, res) {
-  const dbClient = await database.getNewClient();
-  const migrationsConfig = {
-    dbClient,
-    migrationsTable: "pgmigrations",
-    direction: "up",
-    dir: join("infra", "migrations"),
-    verbose: true,
-  };
-
-  if (req.method == "GET") {
-    migrationsConfig.dryRun = true;
-    const pendingMigrations = await migrationRunner(migrationsConfig);
-    dbClient.end();
-    return res.status(200).json(pendingMigrations);
+  const allowedMethods = ["POST", "GET"];
+  if (!allowedMethods.includes(req.method)) {
+    return res.status(405).json({
+      error: `Method ${req.method} is not allowed`,
+    });
   }
 
-  if (req.method == "POST") {
-    const migratedMigrations = await migrationRunner(migrationsConfig);
-    dbClient.end();
+  let dbClient;
+  try {
+    dbClient = await database.getNewClient();
 
-    if (migratedMigrations.length > 0) {
-      return res.status(201).json(migratedMigrations);
+    const migrationsConfig = {
+      dbClient,
+      migrationsTable: "pgmigrations",
+      direction: "up",
+      dir: join("infra", "migrations"),
+      verbose: true,
+    };
+
+    if (req.method == "GET") {
+      migrationsConfig.dryRun = true;
+      const pendingMigrations = await migrationRunner(migrationsConfig);
+      return res.status(200).json(pendingMigrations);
     }
-
-    return res.status(200).json(migratedMigrations);
+    if (req.method == "POST") {
+      const migratedMigrations = await migrationRunner(migrationsConfig);
+      if (migratedMigrations.length > 0) {
+        return res.status(201).json(migratedMigrations);
+      }
+      return res.status(200).json(migratedMigrations);
+    }
+  } catch (e) {
+    console.error(e);
+    throw e;
+  } finally {
+    await dbClient.end();
   }
-
-  return res.status(405).end();
 }
--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -1,31 +1,32 @@
 import database from "infra/database";
 
+beforeAll(cleanSchema);
+
 async function cleanSchema() {
   await database.query("DROP SCHEMA public cascade; CREATE SCHEMA public;");
 }
 
-beforeAll(cleanSchema);
-
-test("A first POST to api/v1/migrations should get at least one migration", async () => {
-  const firsResponse = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "POST",
-  });
+test("The first POST to api/v1/migrations should get at least one migration", async () => {
+  const firsResponse = await migrationPostRequest();
   expect(firsResponse.status).toBe(201);
+
   const firsResponseBody = await firsResponse.json();
   expect(Array.isArray(firsResponseBody)).toBe(true);
   expect(firsResponseBody.length).toBeGreaterThan(0);
 });
 
-test("A second Post to api/v1/migrations should get 0 migrations left", async () => {
-  const secondResponse = await fetch(
-    "http://localhost:3000/api/v1/migrations",
-    {
-      method: "POST",
-    },
-  );
+test("The second POST to api/v1/migrations should get 0 migrations left", async () => {
+  const secondResponse = await migrationPostRequest();
   expect(secondResponse.status).toBe(200);
 
   const secondResponseBody = await secondResponse.json();
   expect(Array.isArray(secondResponseBody)).toBe(true);
   expect(secondResponseBody.length).toEqual(0);
 });
+
+async function migrationPostRequest() {
+  const migrationsApiURL = "http://localhost:3000/api/v1/migrations";
+  return await fetch(migrationsApiURL, {
+    method: "POST",
+  });
+}
